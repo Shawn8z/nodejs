07a36c837fc87881bfc41c9232897a02b2b4fd06
Shawn <lcorel86@gmail.com>
2022-11-01 23:34:58 -0500

    starting navbar-app

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,17 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+// get all files
+const homePage = readFileSync("./navbar-app/index.html");
 
 const server = http.createServer((req, res)=>{
     const url = req.url;
-
+    console.log(url);
+    
     // home page
     if (url === "/") {
         res.writeHead(200, {"content-type":"text/html"})
-        res.write("<h1>home page</h1>");
+        res.write(homePage);
         res.end();
     }
     // about page
