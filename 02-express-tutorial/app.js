af5711150ce2b72ccd211ea0737704ce9b6430e7
Shawn <lcorel86@gmail.com>
2022-11-06 00:01:08 -0500

    all static

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,10 +3,14 @@ const path = require("path");
 
 const app = express();
 
+// setup static and middleware
+app.use(express.static("./public"));
 
-app.get("/",(req, res)=> {
-    res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
-})
+// app.get("/",(req, res)=> {
+//     res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
+// adding to static assets
+// server side rendering
+// })
 
 app.all("*", (req, res)=> {
     res.status(404).send("resource not found");
@@ -15,4 +19,4 @@ app.all("*", (req, res)=> {
 
 app.listen(5000, ()=> {
     console.log("server is listening on port 5000....");
-})
+}) 
