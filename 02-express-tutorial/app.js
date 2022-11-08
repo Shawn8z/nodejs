19c5e2e3ee0ee1e535460b1ee8fc4425340f676e
Shawn <lcorel86@gmail.com>
2022-11-07 20:10:37 -0600

    at POST form example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,28 +1,13 @@
 const express = require("express")
 const app = express()
-const morgan = require("morgan")
-const logger = require("./logger")
-const authorize = require("./authorize")
-// req => middleware => res
+let {people} = require("./data")
 
-app.use(morgan("tiny"))
-// app.use([authorize, logger])
+// static asses
+app.use(express.static("./methods-public"))
 
-app.get("/", (req, res)=>{
-    res.send("Home")
-})
 
-app.get("/about", (req, res)=> {
-    res.send("About")
-})
-
-app.get("/api/products", (req, res)=> {
-    res.send("Products")
-})
-
-app.get("/api/items", (req, res)=> {
-    console.log(req.user)
-    res.send("Items")
+app.get("/api/people", (req, res)=> {
+    res.status(200).json({success:true, data:people})
 })
 
 app.listen(5000, ()=> {
