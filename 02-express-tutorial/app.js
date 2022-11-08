b3445d57f6bd9c1dc51aacfa53b49b0c546a4b59
Shawn <lcorel86@gmail.com>
2022-11-07 18:49:37 -0600

    added logger

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const app = express()
-
+const logger = require("./logger")
 // req => middleware = > res
 
+
+app.use(logger)
+
+
 app.get("/", (req, res)=>{
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear();
-    console.log(method, url, time)
     res.send("Home")
 })
 
@@ -15,6 +15,13 @@ app.get("/about", (req, res)=> {
     res.send("About")
 })
 
+app.get("/api/products", (req, res)=> {
+    res.send("Products")
+})
+
+app.get("/api/items", (req, res)=> {
+    res.send("Items")
+})
 
 app.listen(5000, ()=> {
     console.log("Server is listening on port 5000....")
