1ade7d3eb8a52b8b7b1fdd2e137b1c981540e981
Shawn <lcorel86@gmail.com>
2022-11-11 02:04:37 -0600

    starting task manager, at 12:48

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+
+// routes
+app.get("/hello", (req, res)=>{
+    res.send("Task Manager App");
+})
+
+// app.get("/api/v1/tasks")        -- get all the tasks
+// app.post("/api/v1/task")        -- create a new task
+// app.get("/api/v1/tasks/:id")    -- get single task
+// app.patch("/api/v1/tasks/:id")  -- update task
+// app.delete("/api/v1/task/:id")  -- delete task
+
+
+const port = 3000
+
+app.listen(port, console.log(`Server is listening on ${port}...`))
