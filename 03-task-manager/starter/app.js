859b015eeb221b9eadb2be3010b29f004305fc57
Shawn <lcorel86@gmail.com>
2022-11-11 19:13:51 -0600

    adding initial routes for 03

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks")
 
+// middleware
 
-// routes
+app.use(express.json())
+
 app.get("/hello", (req, res)=>{
     res.send("Task Manager App");
 })
+
+app.use("/api/v1/task", tasks)
+
 
 // app.get("/api/v1/tasks")        -- get all the tasks
 // app.post("/api/v1/task")        -- create a new task
