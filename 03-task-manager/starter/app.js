26022f0e52eb669b6eadf3925a7aeefd8de378ac
Shawn <lcorel86@gmail.com>
2022-11-11 19:39:26 -0600

    basic routes in, making checkmark

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -14,10 +14,10 @@ app.use("/api/v1/task", tasks)
 
 
 // app.get("/api/v1/tasks")        -- get all the tasks
-// app.post("/api/v1/task")        -- create a new task
+// app.post("/api/v1/tasks")        -- create a new task
 // app.get("/api/v1/tasks/:id")    -- get single task
 // app.patch("/api/v1/tasks/:id")  -- update task
-// app.delete("/api/v1/task/:id")  -- delete task
+// app.delete("/api/v1/tasks/:id")  -- delete task
 
 
 const port = 3000
