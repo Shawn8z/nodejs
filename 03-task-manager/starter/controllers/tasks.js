5a2ddc7d5f637e2884bad1b1b9007509fd1961c7
Shawn <lcorel86@gmail.com>
2022-11-15 02:03:31 -0600

    adding asyncWrapper

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,13 +1,15 @@
 const Task = require("../models/Task")
+const asyncWrapper = require("../middleware/async")
 
-const getAllTasks = async (req, res) => {
+
+const getAllTasks = asyncWrapper( async (req, res) => {
     try {
         const tasks = await Task.find({})
         res.status(200).json({ tasks })
     } catch (error) {
         res.status(500).json({ msg: error })
     }
-}
+})
 
 const createTask = async (req, res) => {
     try {
@@ -66,10 +68,32 @@ const updateTask = async (req, res) => {
     }
 }
 
+const editTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        if (!task) {
+            return res.status(404).json({ msg: `No task found with id: ${taskID}` })
+        }
+
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+
+
+
 module.exports = {
     getAllTasks,
     createTask,
     getTask,
     updateTask,
-    deleteTask
+    deleteTask,
+    editTask
 }
