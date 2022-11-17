240df22d266bee3291867595070bb7ff29146b67
Shawn <lcorel86@gmail.com>
2022-11-16 23:46:48 -0600

    at express async errors

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
+require("express-async-errors")
 // async errors
 
 const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect")
+const productsRouter = require("./routes/products")
 
 const notFoundMiddleware = require("./middleware/not-found")
 const errorMiddleware = require("./middleware/error-handler")
@@ -15,16 +17,15 @@ app.use(express.json())
 
 
 // routes
-
 app.get("/", (req, res) => {
     res.send(`<h1>Store API</h1><a href="/api/v1/products">products route</a>`)
 })
 
-app.use("./api/v1/products", )
+
+app.use("/api/v1/products", productsRouter)
 
 
 // products route
-
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
