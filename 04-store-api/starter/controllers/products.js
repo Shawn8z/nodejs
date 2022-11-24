7b78dcdcdcbd6705423c2f7480b50b1f2ca5040f
Shawn <lcorel86@gmail.com>
2022-11-24 00:11:50 -0600

    at adding filter to numirical values

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -2,12 +2,14 @@ const Product = require("../models/product")
 
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).select("name price")
+    const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price")
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -30,6 +32,19 @@ const getAllProducts = async (req, res) => {
     } else {
         result = result.sort("createdAt")
     }
+
+    if (fields) {
+        const fieldsList = fields.split(",").join(" ")
+        result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+
+
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
