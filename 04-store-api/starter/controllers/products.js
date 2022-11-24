41b2778ecc13ad5880427e824822cffaf752ba5f
Shawn <lcorel86@gmail.com>
2022-11-24 01:41:49 -0600

    04 store api done

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -9,8 +9,10 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
+
+
 
     if (featured) {
         queryObject.featured = featured === "true" ? true : false 
@@ -24,8 +26,31 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: "i" }
     }
 
-    // console.log(queryObject)
+    if (numericFilters) {
+        const operatorMap = {
+            ">": "$gt",
+            ">=": "$gte",
+            "=": "$eq",
+            "<": "$lt",
+            "<=": "$lte"
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+        
+        const options = ["price", "rating"];
+        filters = filters.split(",").forEach((item)=>{
+            const [field, operator, value] = item.split("-");
+            if (options.includes(field)){
+                queryObject[field] = { [operator]:Number(value) }
+            }
+        })
+    }
+
+
+    console.log(queryObject)
     let result = Product.find(queryObject)
+
+    // sort
     if (sort) {
         const sortList = sort.split(",").join(" ")
         result = result.sort(sortList)
